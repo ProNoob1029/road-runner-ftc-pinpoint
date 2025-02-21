61d23724142ebefbc9a1c977a886e5ad48a5e060
ProNoob1029 <pronoob1029@gmail.com>
2025-02-21 22:00:41 +0200

    hope it works

--- web/tuning/common.ts
+++ web/tuning/common.ts
@@ -79,7 +79,7 @@ function fixVels(ts: number[], xs: number[], vs: number[]) {
     throw new Error(`${ts.length} !== ${xs.length} !== ${vs.length}`);
   }
 
-  return numDerivOffline(ts, xs).map((est, i) => inverseOverflow(vs[i + 1], est));
+  return numDerivOffline(ts, xs).map((est, i) => {return (vs[i + 1], est)});
 }
 
 type RegressionOptions = {
